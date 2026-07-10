782ebcecb35f25ebe03d930d8f1278843312aede
Tushar Ameria <tusharameria@gmail.com>
2026-07-10 18:39:56 +0530

    first canvas

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    My first game
-  </div>
-`
+const canvas = document.getElementById("game") as HTMLCanvasElement;
+
+if (!canvas) {
+  throw new Error("Canvas not found");
+}
+
+const ctx = canvas.getContext("2d");
+
+if (!ctx) {
+  throw new Error("Could not get 2D context");
+}
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.fillStyle = "#1f2937";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
