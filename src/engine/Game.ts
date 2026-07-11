74dada1fd8ecdb413b3850a961b7fde394bfc9d5
Tushar Ameria <tusharameria@gmail.com>
2026-07-11 14:56:19 +0530

    always update and then render

--- src/engine/Game.ts
+++ src/engine/Game.ts
@@ -1,6 +1,7 @@
 export class Game {
   private readonly canvas: HTMLCanvasElement
   private readonly ctx: CanvasRenderingContext2D
+  private frameCount = 0;
 
   constructor(
     canvas: HTMLCanvasElement,
@@ -14,14 +15,22 @@ export class Game {
     this.loop();
   }
 
+  private update(): void {
+    this.frameCount++
+  }
+
   private render(): void {
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
     this.ctx.fillStyle = `hsl(${Date.now() / 20}, 100%, 50%)`;
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fillStyle = "black";
+    this.ctx.font = "24px sans-serif";
+    this.ctx.fillText(`Frame: ${this.frameCount}`, 20, 40);
   }
 
   private loop(): void {
+    this.update();
     this.render();
     requestAnimationFrame(() => this.loop());
   }
