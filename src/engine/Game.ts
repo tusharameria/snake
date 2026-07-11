28b148f2cadf1fc837b17510c9be81acc9b387cb
Tushar Ameria <tusharameria@gmail.com>
2026-07-11 16:37:59 +0530

    concept of time in game, delta time, timestamp

--- src/engine/Game.ts
+++ src/engine/Game.ts
@@ -1,8 +1,10 @@
 export class Game {
   private readonly canvas: HTMLCanvasElement;
   private readonly ctx: CanvasRenderingContext2D;
-  private frameCount = 0;
+  private stepCount = 0;
   private lastFrameTime = 0;
+  private timeElapsed = 0;
+  private timePerStep = 150;
 
   constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
     this.canvas = canvas;
@@ -14,8 +16,11 @@ export class Game {
   }
 
   private update(deltaTime: number): void {
-    // this.frameCount = Number((timestamp/60).toFixed(0));
-    // this.lastFrameTime = timestamp;
+    this.timeElapsed += deltaTime;
+    while (this.timeElapsed >= this.timePerStep) {
+      this.stepCount++;
+      this.timeElapsed -= this.timePerStep;
+    }
   }
 
   private render(): void {
@@ -23,7 +28,7 @@ export class Game {
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.fillStyle = 'black';
     this.ctx.font = '24px sans-serif';
-    this.ctx.fillText(`Frame: ${this.frameCount}`, 20, 40);
+    this.ctx.fillText(`Step: ${this.stepCount}`, 20, 40);
   }
 
   private readonly loop = (timestamp: number): void => {
