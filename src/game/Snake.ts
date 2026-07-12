f65a8bab207d60c8dbb6863522cda55ac4ae2ce1
Tushar Ameria <tusharameria@gmail.com>
2026-07-12 20:42:43 +0530

    snake reverse direction if pressed too fast bug fixed

--- src/game/Snake.ts
+++ src/game/Snake.ts
@@ -4,7 +4,8 @@ import type { Position } from './Position';
 
 export class Snake {
   private body: Position[];
-  private direction: Direction;
+  private currentDirection: Direction;
+  private queuedDirection: Direction;
   private pendingGrowth: number;
 
   public constructor() {
@@ -13,20 +14,22 @@ export class Snake {
       { x: 4, y: 5 },
       { x: 5, y: 5 },
     ];
-    this.direction = DIRECTIONS.Right;
+    this.currentDirection = DIRECTIONS.Right;
     this.pendingGrowth = 0;
+    this.queuedDirection = this.currentDirection;
   }
 
   public get segments(): readonly Position[] {
     return this.body;
   }
 
-  public setDirection(direction: Direction): void {
-    const movingVertically = this.direction.dy !== 0;
-    const newDirectionIsVertical = direction.dy !== 0;
+  public setDirection(currentDirection: Direction): void {
+    if (this.currentDirection != this.queuedDirection) return;
+    const movingVertically = this.queuedDirection.dy !== 0;
+    const newDirectionIsVertical = currentDirection.dy !== 0;
 
     if (movingVertically !== newDirectionIsVertical) {
-      this.direction = direction;
+      this.queuedDirection = currentDirection;
     }
   }
 
@@ -34,12 +37,13 @@ export class Snake {
     const head = this.body[this.body.length - 1];
 
     return {
-      x: (head.x + this.direction.dx + GRID_WIDTH) % GRID_WIDTH,
-      y: (head.y + this.direction.dy + GRID_HEIGHT) % GRID_HEIGHT,
+      x: (head.x + this.queuedDirection.dx + GRID_WIDTH) % GRID_WIDTH,
+      y: (head.y + this.queuedDirection.dy + GRID_HEIGHT) % GRID_HEIGHT,
     };
   }
 
   public move(): void {
+    this.currentDirection = this.queuedDirection;
     const nextHead = this.getNextHeadPosition();
     if (this.pendingGrowth == 0) {
       this.body.shift();
@@ -59,7 +63,8 @@ export class Snake {
       { x: 4, y: 5 },
       { x: 5, y: 5 },
     ];
-    this.direction = DIRECTIONS.Right;
+    this.currentDirection = DIRECTIONS.Right;
+    this.queuedDirection = this.currentDirection;
     this.pendingGrowth = 0;
   }
 
